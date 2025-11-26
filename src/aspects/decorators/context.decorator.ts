c8d52feac74301cf7991cdaa12f8e7ccbf265dbd
iovitz <iovitz1999@gmail.com>
2025-11-26 10:09:37 +0800

    feat: 增加一些获取请求上下文的decorator

---
+++ src/aspects/decorators/context.decorator.ts
@@ -0,0 +1,28 @@
+import { createParamDecorator, ExecutionContext, UnauthorizedException } from '@nestjs/common'
+
+// 获取用户IP
+export const CurrentUser = createParamDecorator(
+  (ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest<FastifyRequest>()
+    const user = req.user
+    if (!user) {
+      throw new UnauthorizedException()
+    }
+    return user
+  },
+)
+
+// 获取用户IP
+export const ClientIP = createParamDecorator(
+  (data: string, ctx: ExecutionContext) => {
+    const req = ctx.switchToHttp().getRequest<FastifyRequest>()
+
+    const ip = req.cookies.session
+      ?? req.cookies['client-id']
+      ?? req.ip ?? req.socket?.remoteAddress ?? req.headers['x-forwarded-for']
+    if (!ip) {
+      throw new Error('Invalid Request')
+    }
+    return ip.match(/\d+\.\d+\.\d+\.\d+/)?.[0]
+  },
+)
